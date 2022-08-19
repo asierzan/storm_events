532f1594f11ee22e163873ba1981b1bff5e326e2
asierzan <asierzan@gmail.com>
2022-08-19 14:59:28 +0200

    Add entity, controller, service and repository classes, add basic get and add operations

--- participants/src/test/java/com/storm_events/participants/ParticipantApplicationTests.java
+++ participants/src/test/java/com/storm_events/participants/ParticipantApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ParticipantsApplicationTests {
+class ParticipantApplicationTests {
 
 	@Test
 	void contextLoads() {
