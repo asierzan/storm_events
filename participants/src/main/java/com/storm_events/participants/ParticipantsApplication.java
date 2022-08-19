b8a72f53d3b2bdd3c9974b812004b993d6711b68
asierzan <asierzan@gmail.com>
2022-08-19 12:50:03 +0200

    Add module participants

---
+++ participants/src/main/java/com/storm_events/participants/ParticipantsApplication.java
@@ -0,0 +1,13 @@
+package com.storm_events.participants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParticipantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParticipantsApplication.class, args);
+	}
+
+}
